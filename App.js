756d639023487e2a9e96d94073bd7ff6a3e5c95a
3ml12T <adahrhong@gmail.com>
2021-01-17 05:19:07 -0500

    Colors

--- App.js
+++ App.js
@@ -10,9 +10,8 @@ import {
   Button,
   ScrollView,
 } from 'react-native';
-//import Icon from '@mdi/font';
-import { mdiMenu } from '@mdi/js';
-import WordBlock from './components/WordBlock';
+
+import WordBlock from './components/Word';
 
 var { height } = Dimensions.get('window');
 
@@ -28,25 +27,34 @@ export default class VerticalStackLayout extends Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-        <View style={[styles.box, styles.box1]}>
-            <Image
-              source={(uri = 'https://img.icons8.com/cotton/2x/menu.png')}
-              style={styles.logo}
-            />
+          <View style={[styles.box, styles.box1]}>
+            <TouchableOpacity>
+              <Image
+                source={require('./assets/menu.png')}
+                style={styles.logo}
+              />
+            </TouchableOpacity>
             <Text style={styles.TitleText} numberOfLines={1}>
-              Dictionary 1 - French
+              Vocab for Writing
             </Text>
           </View>
           <View style={[styles.box, styles.box2]}>
             <Text style={styles.letter}>D</Text>
             <WordBlock word="Dashing" definition="ahhh"/>
             <WordBlock word="Delivery" definition="I am delivering food oh such good food yummy yummy yummy"/>
+            
             <Text style={styles.letter}>F</Text>
             <WordBlock word="Fashion" definition="ahhh"/>
             <WordBlock word="Friendship" definition="hehehaha we know each other welllll"/>
             <WordBlock word="Forbid" definition="never never not allowed angeryyyy"/>
             <WordBlock word="Fundraise" definition="money money gib money please i promise its for a good cause maybe not but moonnnennyyyyy"/>
-        </View>
+            <WordBlock word="Fundraise" definition="money money gib money please i promise its for a good cause maybe not but moonnnennyyyyy"/>
+            <WordBlock word="Fashion" definition="ahhh"/>
+            <WordBlock word="Friendship" definition="hehehaha we know each other welllll"/>
+            <WordBlock word="Forbid" definition="never never not allowed angeryyyy"/>
+            <WordBlock word="Fundraise" definition="money money gib money please i promise its for a good cause maybe not but moonnnennyyyyy"/>
+            
+          </View>
         </View>
       </ScrollView>
     );
@@ -54,22 +62,49 @@ export default class VerticalStackLayout extends Component {
 }
 
 const styles = StyleSheet.create({
+    like: {
+    width: 15,
+    height: 15,
+    borderRadius: 15 / 2,
+    marginLeft: 300,
+    marginTop: -20,
+  },
+  definitions: {
+    fontSize: 10,
+    marginRight: 45,
+    marginTop: -30,
+    marginBottom: 0,
+  },
+  words: {
+    marginLeft: 10,
+    marginTop: 5,
+  },
+  letter: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    paddingBottom: 4,
+    fontSize: 15,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
   logo: {
     width: 35,
     height: 35,
-    borderRadius: 30 / 2,
+    borderRadius: 35 / 2,
     justifyContent: 'top',
     alightItems: 'right',
     marginTop: 10,
     marginLeft: 10,
+    marginBottom: 10,
   },
   TitleText: {
     fontSize: 20,
-    color: 'black',
+    color: 'white',
     textAlign: 'left',
-    marginTop: -30,
+    marginTop: -40,
     marginLeft: 55,
     flexWrap: 'wrap',
+    marginBottom:10,
   },
   container: {
     flex: 1,
@@ -81,7 +116,7 @@ const styles = StyleSheet.create({
   },
   box1: {
     flex: 1.5,
-    backgroundColor: '#EBEBEB',
+    backgroundColor: '#2463FC',
   },
   box2: {
     flex: 3,
